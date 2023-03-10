a2ae07e5eaf7473500989a59335d61d38b7eff99
ServEr <Susurkaev@yandex.ru>
2023-03-10 16:18:36 +0300

    Поиск сцммы элементов массива стоящих на четных позициях

---
+++ Задача_36/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+System.Console.WriteLine("Укажите размерность массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] array = new int[arraySize];
+
+Library.FillArray(array);
+System.Console.WriteLine("");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+System.Console.Write("Количество четных числе в массиве равно: " + count);
